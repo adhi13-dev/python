d85b3a1fd6f088144fab556d0def434cb0932972
adhi13-dev <90518972+adhi13-dev@users.noreply.github.com>
2021-09-14 19:45:22 +0530

    Add files via upload

---
+++ c99pro.py
@@ -0,0 +1,23 @@
+inputsentence = ""
+outputsentence = ""
+input_path = "C:\Users\srive\OneDrive\Documents\whitehat\c99pros.txt"
+output_path = "C:\Users\srive\OneDrive\Documents\whitehat\c99prod.txt"
+def read(path):
+    file = open(path,"r")
+    inputsentence = file.read()
+    file.close()
+def write(path):
+    file = open(path,"w")
+    if path == input_path:
+        file.write(outputsentence)
+    else:
+        file.write(inputsentence)
+    file.close()
+#read 1st file
+read(input_path)
+#read 2nd file
+read(output_path)
+#write 1st file
+write(input_path)
+#write 2nd file
+write(output_path)
